6ae357f31c1c6b6f41d04540966deb8af8a1dd33
Gustavo Santiago <gustavohenriquesantiago5@gmail.com>
2026-04-16 20:29:06 -0300

    feat: add Zustand state management for grocery items and update dependencies

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,16 +1,24 @@
+import { useGroceryStore } from "@/store/grocery-store";
 import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
 import { Badge, Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import { useColorScheme } from "nativewind";
+import { useEffect } from "react";
 
 export default function TabsLayout() {
   const { isSignedIn, isLoaded } = useAuth();
+  const {loadItems, items} = useGroceryStore();
   const {colorScheme} = useColorScheme();
   const isDark = colorScheme === "dark";
   const tabTintColor = isDark ? "hsl(142 70% 54%)" : "hsl(147 75% 33%)";
 
+  useEffect( () => {
+    loadItems();
+  }, [] )
+
   if (!isLoaded) return null;
-  if (!isSignedIn) return <Redirect href="../(auth)/sign-in" />;
+  if (!isSignedIn) return <Redirect href="/(auth)/sign-in" />;
+    // ../(auth)/sign-in
 
   return (
     <NativeTabs tintColor={tabTintColor}>
